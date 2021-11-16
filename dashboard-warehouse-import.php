cea4f7db063220dd13f0846957ffc5a0ff4d1a13
Việt Trung <73702724+namvt002@users.noreply.github.com>
2021-11-16 22:01:57 +0700

    new

---
+++ dashboard-warehouse-import.php
@@ -0,0 +1,355 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>A&T Shop Admin - Dashboard</title>
+
+    <!-- Custom fonts for this template-->
+    <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
+
+    <!-- Custom styles for this template-->
+    <link href="css/sb-admin-2.min.css" rel="stylesheet">
+
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+
+</head>
+
+<body id="page-top">
+
+    <!-- Page Wrapper -->
+    <div id="wrapper">
+
+        <!-- Sidebar -->
+        <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
+
+            <!-- Sidebar - Brand -->
+            <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index-admin.php">
+                <div class="sidebar-brand-icon rotate-n-15">
+                    <i class="fas fa-laugh-wink"></i>
+                </div>
+                <div class="sidebar-brand-text mx-3">A&T Shop Admin </div>
+            </a>
+
+            <!-- Divider -->
+            <hr class="sidebar-divider my-0">
+
+            <!-- Nav Item - Dashboard -->
+            <li class="nav-item active">
+                <a class="nav-link" href="index-admin.php   ">
+                    <i class="fas fa-fw fa-tachometer-alt"></i>
+                    <span>Dashboard</span></a>
+            </li>
+
+            <!-- Divider -->
+            <hr class="sidebar-divider">
+
+            <!-- Heading -->
+            <div class="sidebar-heading">
+                Interface
+            </div>
+
+            <li class="nav-item">
+                <a class="nav-link" href="dashboard-order.php">
+                    <i class="fas fa-fw fa-table"></i>
+                    <span>Đơn hàng</span></a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="dashboard-product.php">
+                    <i class="fas fa-fw fa-table"></i>
+                    <span>Sản phẩm</span></a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="dashboard-product-details.php">
+                    <i class="fas fa-fw fa-table"></i>
+                    <span>Chi tiết sản phẩm</span></a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="dashboard-warehouse.php">
+                    <i class="fas fa-fw fa-table"></i>
+                    <span>Kho hàng</span></a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="dashboard-warehouse-import.php">
+                    <i class="fas fa-fw fa-table"></i>
+                    <span>Nhập hàng</span></a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="dashboard-promotion.php">
+                    <i class="fas fa-fw fa-table"></i>
+                    <span>Khuyến mãi</span></a>
+            </li>
+            <!-- Divider -->
+            <hr class="sidebar-divider d-none d-md-block">
+
+            <!-- Sidebar Toggler (Sidebar) -->
+            <div class="text-center d-none d-md-inline">
+                <button class="rounded-circle border-0" id="sidebarToggle"></button>
+            </div>
+
+
+
+        </ul>
+        <!-- End of Sidebar -->
+
+        <!-- Content Wrapper -->
+        <div id="content-wrapper" class="d-flex flex-column">
+
+            <!-- Main Content -->
+            <div id="content">
+
+                <!-- End of Topbar -->
+
+                <!-- Topbar -->
+                <nav class="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow">
+
+                    <!-- Sidebar Toggle (Topbar) -->
+                    <button id="sidebarToggleTop" class="btn btn-link d-md-none rounded-circle mr-3">
+                        <i class="fa fa-bars"></i>
+                    </button>
+
+
+                    <!-- Topbar Navbar -->
+                    <ul class="navbar-nav ml-auto">
+
+                        <!-- Nav Item - Search Dropdown (Visible Only XS) -->
+                        <li class="nav-item dropdown no-arrow d-sm-none">
+                            <a class="nav-link dropdown-toggle" href="#" id="searchDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                <i class="fas fa-search fa-fw"></i>
+                            </a>
+                            <!-- Dropdown - Messages -->
+                            <div class="dropdown-menu dropdown-menu-right p-3 shadow animated--grow-in" aria-labelledby="searchDropdown">
+                                <form class="form-inline mr-auto w-100 navbar-search">
+                                    <div class="input-group">
+                                        <input type="text" class="form-control bg-light border-0 small" placeholder="Search for..." aria-label="Search" aria-describedby="basic-addon2">
+                                        <div class="input-group-append">
+                                            <button class="btn btn-primary" type="button">
+                                                <i class="fas fa-search fa-sm"></i>
+                                            </button>
+                                        </div>
+                                    </div>
+                                </form>
+                            </div>
+                        </li>
+
+
+                        <div class="topbar-divider d-none d-sm-block"></div>
+
+                        <!-- Nav Item - User Information -->
+                        <li class="nav-item dropdown no-arrow">
+                            <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                <span class="mr-2 d-none d-lg-inline text-gray-600 small">Admin</span>
+                                <img class="img-profile rounded-circle" src="img/undraw_profile.svg">
+                            </a>
+                            <!-- Dropdown - User Information -->
+                            <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="userDropdown">
+                                <a class="dropdown-item" href="#">
+                                    <i class="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i>
+                                    Profile
+                                </a>
+                                <a class="dropdown-item" href="#">
+                                    <i class="fas fa-cogs fa-sm fa-fw mr-2 text-gray-400"></i>
+                                    Settings
+                                </a>
+                                <a class="dropdown-item" href="#">
+                                    <i class="fas fa-list fa-sm fa-fw mr-2 text-gray-400"></i>
+                                    Activity Log
+                                </a>
+                                <div class="dropdown-divider"></div>
+                                <a class="dropdown-item" href="#" data-toggle="modal" data-target="#logoutModal">
+                                    <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
+                                    Logout
+                                </a>
+                            </div>
+                        </li>
+
+                    </ul>
+
+                </nav>
+
+                <!-- /.container-fluid -->
+
+                <!-- code in here -->.
+                <div class="container-fluid">
+                    <div class="d-sm-flex align-items-center justify-content-between mb-4">
+                        <h1 class="h3 mb-0 text-gray-800">Nhập hàng</h1>
+
+                    </div>
+                    <div class="d-sm-flex align-items-center justify-content-between mb-4">
+                        <form method="post" action="">
+                            <div class="input-group">
+                                <input type="text" class="form-control bg-light border-1 small" placeholder="Search " aria-label="Search" aria-describedby="basic-addon2">
+                                <div class="input-group-append">
+                                    <button class="btn btn-primary" type="button">
+                                        <i class="fas fa-search fa-sm"></i>
+                                    </button>
+                                </div>
+
+                            </div>
+                        </form>
+
+                        <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal"><i class="fas fa-download fa-sm text-white-50"></i>Thêm mới nhập hàng</button>
+
+                    </div>
+
+
+
+                    <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                        <div class="modal-dialog">
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                    <h5 class="modal-title" id="exampleModalLabel">Thêm mới nhập hàng</h5>
+                                </div>
+                                <div class="modal-body">
+                                    <form action="" method="post" enctype="multipart/form-data">
+                                        <div class="mb-3">
+                                            <label for="recipient-name" class="col-form-label">Tên sản phẩm:</label>
+                                            <input type="text" name="name_product" class="form-control" placeholder="Nhập tên sản phẩm">
+                                        </div>
+                                        <div class="mb-3">
+                                            <label>Loại sản phẩm</label> <br>
+                                            <select class="form-select" aria-label="Default select example" name="Loai_Hang">
+                                                <option value="">---Chọn loại sản phẩm---</option>
+                                                <?php
+                                                    // while ($row = $result->fetch_assoc()) {
+                                                    //     echo " <option value=" . $row['LH_MA'] . ">";
+                                                    //     echo   $row['LH_TEN'];
+                                                    //     echo " </option>";
+                                                    // }
+                                                ?>
+                                            </select>
+                                        </div>
+
+                                        <div class="mb-3">
+                                            <label for="recipient-name" class="col-form-label">Chọn hình ảnh:</label>
+                                            <input type="file" name="imgProduct" class="form-control">
+                                        </div>
+
+                                        <div class="mb-3">
+                                            <label>Nhà sản xuất</label> <br>
+                                            <select class="form-select" aria-label="Default select example" name="Nha_San_Xuat">
+                                                <option value="">---Chọn loại nhà sản xuất---</option>
+                                                <?php
+                                               
+                                                    // while ($rowNSX = $resultNSX->fetch_assoc()) {
+                                                    //     echo " <option value=" . $rowNSX['NSX_MA'] . ">";
+                                                    //     echo   $rowNSX['NSX_TEN'];
+                                                    //     echo " </option>";
+                                                    // }
+                                                ?>
+                                            </select>
+                                        </div>
+
+                                        <div class="mb-3">
+                                            <label for="recipient-name" class="col-form-label">Giá:</label>
+                                            <input type="text" name="price" class="form-control" placeholder="Nhập giá">
+                                        </div>
+                                    </form>
+                                </div>
+                                <div class="modal-footer">
+                                    <input type="submit" class="btn btn-primary" value="Thêm mới" name="submit-add">
+                                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Đóng</button>
+
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <table class="table table-hover">
+                        <thead>
+                            <tr>
+                                <th scope="col">#</th>
+                                <th scope="col">First</th>
+                                <th scope="col">Last</th>
+                                <th scope="col">Handle</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <th scope="row">1</th>
+                                <td>Mark</td>
+                                <td>Otto</td>
+                                <td>@mdo</td>
+                            </tr>
+                            <tr>
+                                <th scope="row">2</th>
+                                <td>Jacob</td>
+                                <td>Thornton</td>
+                                <td>@fat</td>
+                            </tr>
+                            <tr>
+                                <th scope="row">3</th>
+                                <td colspan="2">Larry the Bird</td>
+                                <td>@twitter</td>
+                            </tr>
+                        </tbody>
+
+                    </table>
+                </div>
+                <!--  -->
+
+            </div>
+            <!-- End of Main Content -->
+
+
+        </div>
+        <!-- End of Content Wrapper -->
+
+    </div>
+    <!-- End of Page Wrapper -->
+
+    <!-- Scroll to Top Button-->
+    <a class="scroll-to-top rounded" href="#page-top">
+        <i class="fas fa-angle-up"></i>
+    </a>
+
+    <!-- Logout Modal-->
+    <div class="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">Ready to Leave?</h5>
+                    <button class="close" type="button" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">×</span>
+                    </button>
+                </div>
+                <div class="modal-body">Select "Logout" below if you are ready to end your current session.</div>
+                <div class="modal-footer">
+                    <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
+                    <a class="btn btn-primary" href="login.html">Logout</a>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- Bootstrap core JavaScript-->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+    <!-- Core plugin JavaScript-->
+    <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+
+    <!-- Custom scripts for all pages-->
+    <script src="js/sb-admin-2.min.js"></script>
+
+    <!-- Page level plugins -->
+    <script src="vendor/chart.js/Chart.min.js"></script>
+
+    <!-- Page level custom scripts -->
+    <script src="js/demo/chart-area-demo.js"></script>
+    <script src="js/demo/chart-pie-demo.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+
+
+</body>
+
+</html>
---
+++ index.php
@@ -0,0 +1,211 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>A&T Shop</title>
+    <!-- CSS only -->
+    <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
+
+    <!-- Custom styles for this template-->
+    <!-- <link href="css/sb-admin-2.min.css" rel="stylesheet"> -->
+    <link rel="stylesheet" href="css/index.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+
+</head>
+
+<body>
+
+    <section id="main">
+        <?php require "header.php"; ?>
+        <!-- slidershow -->
+        <div id="sliderShow">
+            <div class="container-xxl" style="max-height: 60%;">
+                <div class="row">
+                    <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
+                        <div class="carousel-indicators">
+                            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+                        </div>
+                        <div class="carousel-inner">
+                            <div class="carousel-item active">
+                                <img src="img/client/sliderShow2.jpg" id="img-slider" class="d-block w-100" alt="...">
+                            </div>
+                            <div class="carousel-item">
+                                <img src="img/client/sliderShow1.jpg" id="img-slider" class="d-block w-100" alt="...">
+                            </div>
+                            <div class="carousel-item">
+                                <img src="img/client/sliderShow3.jpg" id="img-slider" class="d-block w-100" alt="...">
+                            </div>
+                        </div>
+                        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+                            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                            <span class="visually-hidden">Previous</span>
+                        </button>
+                        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+                            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                            <span class="visually-hidden">Next</span>
+                        </button>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+
+    </section>
+
+    <!-- coudown sale -->
+
+    <div class="container">
+        <section class="section-center">
+            <!-- image -->
+            <article class="gift-img">
+                <img id="img-save" src="./img/client/save.png" alt="gift" />
+            </article>
+            <!-- info -->
+            <article class="gift-info">
+                <h3>Giveaway Covid</h3>
+                <h4 class="giveaway">
+                    Giveaway on Sunday, 30 september 2021, 23:59PM
+                </h4>
+                <p id="paragraph">
+                    #Win one of FOUR gift certificates to 9thandElm.com, a site devoted to handmade fashion items from amazing designers! $200 for one winner, $100 each to two winners! #giveaway via @simplystacienet ENDS…
+                </p>
+                <div class="deadline">
+
+                    <div class="deadline-format">
+                        <div>
+                            <h4 class="days" class="giveaway1">20</h4>
+                            <span>days</span>
+                        </div>
+                    </div>
+
+                    <div class="deadline-format">
+                        <div>
+                            <h4 class="hours" class="giveaway1">50</h4>
+                            <span>hours</span>
+                        </div>
+                    </div>
+
+                    <div class="deadline-format">
+                        <div>
+                            <h4 class="minutes" class="giveaway1">30</h4>
+                            <span>mins</span>
+                        </div>
+                    </div>
+
+                    <div class="deadline-format">
+                        <div>
+                            <h4 class="seconds">20</h4>
+                            <span>secs</span>
+                        </div>
+                    </div>
+
+                </div>
+            </article>
+        </section>
+
+    </div>
+
+
+    <!-- about -->
+    <div class="container" id="aboutClient">
+        <section class="sectionAbout">
+            <div class="title">
+                <h2>About A&T</h2>
+                <p>
+                    You are my everything
+                </p>
+            </div>
+
+            <div class="about-center section-center">
+                <article class="about-img">
+                    <img src="img/client/A & T.jpg" alt="" />
+                </article>
+                <article class="about">
+                    <!-- btn container -->
+                    <div class="btn-container">
+                        <button class="tab-btn active" data-id="history">history</button>
+                        <button class="tab-btn" data-id="vision">vision</button>
+                        <button class="tab-btn" data-id="goals">goals</button>
+                    </div>
+                    <div class="about-content">
+                        <!-- single item -->
+                        <div class="content active" id="history">
+                            <h4>History</h4>
+                            <p>
+                                I'm baby wolf pickled schlitz try-hard normcore marfa man bun
+                                mumblecore vice pop-up XOXO lomo kombucha glossier bicycle
+                                rights. Umami kinfolk salvia jean shorts offal venmo. Knausgaard
+                                tilde try-hard, woke fixie banjo man bun. Small batch tumeric
+                                mustache tbh wayfarers 8-bit shaman chartreuse tacos. Viral
+                                direct trade hoodie ugh chambray, craft beer pork belly flannel
+                                tacos single-origin coffee art party migas plaid pop-up.
+                                tacos single-origin coffee art party migas plaid pop-up.
+                                tacos single-origin coffee art party migas plaid pop-up.
+                            </p>
+                        </div>
+                        <!-- end of single item -->
+                        <!-- single item -->
+                        <div class="content" id="vision">
+                            <h4>Vision</h4>
+                            <p>
+                                Man bun PBR&B keytar copper mug prism, hell of helvetica. Synth
+                                crucifix offal deep v hella biodiesel. Church-key listicle
+                                polaroid put a bird on it chillwave palo santo enamel pin,
+                                tattooed meggings franzen la croix cray. Retro yr aesthetic four
+                                loko tbh helvetica air plant, neutra palo santo tofu mumblecore.
+                                Hoodie bushwick pour-over jean shorts chartreuse shabby chic.
+                                Roof party hammock master cleanse pop-up truffaut, bicycle
+                                rights skateboard affogato readymade sustainable deep v
+                                live-edge schlitz narwhal.
+                            </p>
+                        </div>
+                        <!-- end of single item -->
+                        <!-- single item -->
+                        <div class="content" id="goals">
+                            <h4>Goals</h4>
+                            <p>
+                                Chambray authentic truffaut, kickstarter brunch taxidermy vape
+                                heirloom four dollar toast raclette shoreditch church-key.
+                                Poutine etsy tote bag, cred fingerstache leggings cornhole
+                                everyday carry blog gastropub. Brunch biodiesel sartorial mlkshk
+                                swag, mixtape hashtag marfa readymade direct trade man braid
+                                cold-pressed roof party. Small batch adaptogen coloring book
+                                heirloom. Letterpress food truck hammock literally hell of wolf
+                                beard adaptogen everyday carry. Dreamcatcher pitchfork yuccie,
+                                banh mi salvia venmo photo booth quinoa chicharrones.
+                            </p>
+                        </div>
+                        <!-- end of single item -->
+                    </div>
+                </article>
+            </div>
+        </section>
+    </div>
+
+    <?php require "footer.php"; ?>
+
+
+    <script src="js/index.js"></script>
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+    <!-- Core plugin JavaScript-->
+    <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+
+    <!-- Custom scripts for all pages-->
+    <script src="js/sb-admin-2.min.js"></script>
+
+    <!-- Page level plugins -->
+    <script src="vendor/chart.js/Chart.min.js"></script>
+    <!-- JavaScript Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+</body>
+
+</html>
